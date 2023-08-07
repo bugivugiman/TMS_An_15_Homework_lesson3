09de0389aec5361d41657165fbe65bddc35b5587
Grouzdev Alexander <71078615+bugivugiman@users.noreply.github.com>
2023-08-07 14:47:57 +0300

    Update Lesson3_1.java
    rewrite code

--- src/Lesson3_1.java
+++ src/Lesson3_1.java
@@ -1,7 +1,8 @@
 public class Lesson3_1 {
     public static void main(String[] args) {
         int a = 34;
-        int b;
-        int c;
+        int b = a + 3;
+        int c = a + b;
+        System.out.println("a = " + a + "; b = "+ b + "; c = " + c);
     }
 }
